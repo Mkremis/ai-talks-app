68c8dfffa24fc8f3b029caef389c6ed81668763c
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-03-27 11:53:23 -0300

    1 file modified

--- src/hooks/useChat.js
+++ src/hooks/useChat.js
@@ -12,14 +12,36 @@ const useChat = () => {
       // apiKey: process.env.REACT_APP_OPENAI_API_KEY,
     });
     const openai = new OpenAIApi(configuration);
-    const response = await openai.createCompletion({
+    function bulletReplace(text) {
+      // Separamos el texto en líneas
+      const lines = text.split('\n');
+      // Expresión regular que busca líneas con viñetas de guiones
+      const regexBullets = /^ *- (.*)$/;
+      // Contador para las enumeraciones
+      let counter = 1;
+      // Recorremos cada línea del texto
+      for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+        // Si la línea tiene viñetas de guiones
+        if (regexBullets.test(line)) {
+          // Reemplazamos la viñeta por una enumeración
+          const newLine = line.replace(regexBullets, ` ${counter}. $1.`);
+          lines[i] = newLine;
+          counter++;
+        }
+      }
+      // Unimos las líneas y devolvemos el texto modificado
+      return lines.join('\n');
+    }
+
+    const res = await openai.createCompletion({
       model: 'text-davinci-003',
       prompt,
       max_tokens: 900,
       temperature: 1,
     });
 
-    setResponse(response.data.choices[0].text);
+    setResponse(bulletReplace(res.data.choices[0].text));
   };
   useEffect(() => {
     if (query) chat(query);

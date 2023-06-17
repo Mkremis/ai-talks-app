1bb7e62e47322ae758972de8bdf4d49d847de6b6
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-06-17 02:00:56 -0300

    Update ResponseAvatar.jsx
    CurrentAvatar

--- src/components/ResponseAvatar.jsx
+++ src/components/ResponseAvatar.jsx
@@ -2,29 +2,19 @@ import React, { useEffect, useState} from "react";
 import Box from "./Box";
 
 const ResponseAvatar = ({ speaking, handleStopSpeak, theme, AVATARS, avatarIndex }) => {
-  const initialAvatar = `/${AVATARS[theme][avatarIndex]}-no_talking.png`
+  const initialAvatar = new Image().src =`/${AVATARS[theme][avatarIndex]}-no_talking.png`
   const [currentAvatar, setCurrentAvatar]=useState(initialAvatar)
-const [currentAvatarAnimated, setCurrentAvatarAnimated]=useState(null)
-useEffect(()=>{
-  const animated = new Image().src = `/${AVATARS[theme][avatarIndex]}-talking.gif`;
-  SetCurrentAvatar = animated;
-},[currentAvatar, SetCurrentAvatarAnimated])
 
   
   useEffect(()=>{
 speaking
-?setCurrentAvatar(currentAvatarAnimated)
-:setCurrentAvatar(`/${AVATARS[theme][avatarIndex]}-no_talking.png`)
+?setCurrentAvatar(new Image().src = `/${AVATARS[theme][avatarIndex]}-talking.gif`)
+:setCurrentAvatar(new Image().src =`/${AVATARS[theme][avatarIndex]}-no_talking.png`)
   },[speaking, setCurrentAvatar, avatarIndex, theme]);
 
   return (
-    <Box text={false} theme={theme}>
-  {speaking ?currentAvatarAnimated :<img
-        className="avatar"
-        src={currentAvatar}
-        alt="Talking face"
-        onClick={handleStopSpeak}
-      />}
+    <Box text={false} theme={theme} onClick={handleStopSpeak>
+      {currentAvatar}
     </Box>
   );
 };

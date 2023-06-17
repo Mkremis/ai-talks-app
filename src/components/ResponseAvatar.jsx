c1aa3e87545cbc6acb004fed63ee5bb4feeda587
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-06-17 10:09:18 -0300

    Update ResponseAvatar.jsx
    image Avatar

--- src/components/ResponseAvatar.jsx
+++ src/components/ResponseAvatar.jsx
@@ -2,23 +2,23 @@ import React, { useEffect, useState} from "react";
 import Box from "./Box";
 
 const ResponseAvatar = ({ speaking, handleStopSpeak, theme, AVATARS, avatarIndex }) => {
-  const initialAvatar = new Image().src =`/${AVATARS[theme][avatarIndex]}-no_talking.png`
+  const initialAvatar = `/${AVATARS[theme][avatarIndex]}-no_talking.png`
   const [currentAvatar, setCurrentAvatar]=useState(initialAvatar)
 
-  
   useEffect(()=>{
 speaking
 ?setCurrentAvatar(`/${AVATARS[theme][avatarIndex]}-talking.gif`)
 :setCurrentAvatar(`/${AVATARS[theme][avatarIndex]}-no_talking.png`)
   },[speaking, setCurrentAvatar, avatarIndex, theme]);
 
- return (
+  return (
     <Box text={false} theme={theme}>
-     <img 
-       src= {currentAvatar} 
-       onClick={handleStopSpeak}
-         />
-      </Box>
+      <img
+        className="avatar"
+        src={currentAvatar}
+        alt="Talking face"
+        onClick={handleStopSpeak}
+      />
     </Box>
   );
 };
